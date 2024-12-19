f6e2183b57fabe9bd2cf1f4e1239437cd4bb4cec
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2024-12-19 11:53:02 +0200

    Add project files.

---
+++ Assignment Session 3/Program.cs
@@ -0,0 +1,212 @@
+﻿using System.Drawing;
+
+namespace Assignment_Session_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q 1
+            //Console.WriteLine("Enter Number :");
+            //int Number = int.Parse(Console.ReadLine()); 
+
+            //if (Number %3 == 0 && Number %4 == 0)
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No");
+            //}
+            #endregion
+            #region Q 2
+            //Console.WriteLine("Enter Integer Number : ");
+            //int Number = int.Parse(Console.ReadLine()); 
+
+            //if (Number < 0)
+            //{
+            //    Console.WriteLine("Negative");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Positive");
+            //}
+            #endregion
+            #region Q 3
+            //Console.WriteLine("Enter the first number : ");
+            //int Num1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter the Second number : ");
+            //int Num2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter the Third number : ");
+            //int Num3 = int.Parse(Console.ReadLine());
+
+            //int max = Num1;
+            //if (Num2 > max ) max = Num2;
+            //if (Num3 > max ) max = Num3;
+
+            //int min = Num1;
+            //if( Num2 < min ) min = Num2;
+            //if( Num3 < min ) min = Num3;
+
+
+            //Console.WriteLine($"Max integer = {max} and Min integer = {min}");
+            #endregion
+            #region Q 4
+            //Console.Write("Enter integer : ");
+            //int number = int.Parse(Console.ReadLine());
+
+            //if (number % 2 == 0)
+            //{
+            //    Console.WriteLine("The number is even.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The number is odd.");
+            //}
+            #endregion
+            #region Q 5
+            //Console.Write("Enter character : ");
+            //char input = Console.ReadKey().KeyChar;     // i dont know the meaning
+            //Console.WriteLine();
+
+            //if (input == 'a' || input == 'e' || input == 'i' || input == 'o' || input == 'u')
+            //{
+            //    Console.WriteLine("vowel");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("consonant");
+            //}
+            #endregion
+            #region Q 7
+            //Console.Write("Enter integer : ");
+            //int number = int.Parse(Console.ReadLine());
+            //if (1 <= 12) Console.Write(number * 1 + " ");        //     " "  for the space
+            //if (2 <= 12) Console.Write(number * 2 + " ");
+            //if (3 <= 12) Console.Write(number * 3 + " ");
+            //if (4 <= 12) Console.Write(number * 4 + " ");
+            //if (5 <= 12) Console.Write(number * 5 + " ");
+            //if (6 <= 12) Console.Write(number * 6 + " ");
+            //if (7 <= 12) Console.Write(number * 7 + " ");
+            //if (8 <= 12) Console.Write(number * 8 + " ");
+            //if (9 <= 12) Console.Write(number * 9 + " ");
+            //if (10 <= 12) Console.Write(number * 10 + " ");
+            //if (11 <= 12) Console.Write(number * 11 + " ");
+            //if (12 <= 12) Console.Write(number * 12);
+            #endregion
+            #region Q 9
+            //Console.WriteLine("Enter Base : ");
+            //int basenum = int.Parse(Console.ReadLine());
+
+            //Console.Write("Enter Exponent : ");
+            //int exponentnum = int.Parse(Console.ReadLine());
+
+
+            #endregion
+            #region Q 10
+            //Console.WriteLine("Enter Marks of five subjects : ");
+            //int mark1 = int.Parse(Console.ReadLine());
+            //int mark2 = int.Parse(Console.ReadLine());
+            //int mark3 = int.Parse(Console.ReadLine());
+            //int mark4 = int.Parse(Console.ReadLine());
+            //int mark5 = int.Parse(Console.ReadLine());
+
+
+            //int Result = mark1 + mark2 + mark3 + mark4 + mark5;
+            //Double average = Result / 5;
+            //Double percentage = (Result / 500.0) * 100;
+
+
+            //Console.WriteLine($"Total marks = {Result}, Average Marks = {average}, Percentage = {percentage}% ");
+            #endregion
+            #region Q 11
+            //Console.Write("Month Number : ");
+            //int month = int.Parse(Console.ReadLine());
+
+
+            //if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+            //{
+            //    Console.WriteLine("Days in Month : 31");
+            //}
+            //else if (month == 4 || month == 6 || month == 9 || month == 11)
+            //{
+            //    Console.WriteLine("Days in Month : 30");
+            //}
+            //else if (month == 2)
+            //{
+
+            //    Console.WriteLine("Days in Month : 28");
+            //}
+            #endregion
+            #region Q 17
+
+            //Console.WriteLine("Enter the coordinates of the first point (x1, y1) : ");
+            //int x1 = int.Parse(Console.ReadLine());
+            //int y1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter the coordinates of the second point (x2, y2) : ");
+            //int x2 = int.Parse(Console.ReadLine());
+            //int y2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter the coordinates of the third point (x3, y3 : ");
+            //int x3 = int.Parse(Console.ReadLine());
+            //int y3 = int.Parse(Console.ReadLine());
+
+
+            //if ((y2 - y1) * (x3 - x2) == (y3 - y2) * (x2 - x1))    // Searched for it i dont know if right
+            //{
+            //    Console.WriteLine(" The points lies on the same straight line ");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The points do not lie on the same straight line ");
+            //}
+            #endregion
+            #region Q 21
+            //int num1 = 20;
+            //int num2 = num1; 
+
+
+            //num2 = 20;
+
+            //Console.WriteLine($"num1: {num1}, num2: {num2}");
+            #endregion
+            #region Q 22
+            //Point P1;
+            //P1 = new Point();
+
+            //Point P2 = new Point();       // now this is unreachable 
+            //P2 = P1;
+
+            //P1.x = 9;
+            //Console.WriteLine(P2.x);          // they both now assign to same address 
+            //Console.WriteLine(P1.x);          // The same Result ..
+            #endregion
+            #region Q 23
+            // The correct answer is (B)
+            //int d;
+            //d = Convert.ToInt32(!(30 < 20));
+
+            #endregion
+            #region Q 24
+            // The Correct answer is (D) >> 6 1
+            //  Console.WriteLine(13 / 2 + " " + 13 % 2);
+            #endregion
+            #region Q 25
+            //int num = 1, z = 5;
+
+
+            //if (!(num <= 0))
+            //    Console.WriteLine(++num + z++ + " " + ++z);
+            //else
+            //    Console.WriteLine(--num + z-- + " " + --z);
+
+            // The Correct answer is (D) >>> 7 7
+
+            #endregion
+
+        }
+    }
+}
